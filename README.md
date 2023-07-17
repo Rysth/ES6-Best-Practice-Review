0d95a336b9ab2d39b811d75b87532cddbfe5befe
Rysth <johnpalacios.t@gmail.com>
2023-07-17 15:07:52 -0500

    Project Initial Commit.

---
+++ README.md
@@ -0,0 +1,128 @@
+<!-- TABLE OF CONTENTS -->
+
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Tech Stack](#tech-stack)
+    - [Key Features](#key-features)
+    - [Future Features](#future-features)
+- [💻 Getting Started](#getting-started)
+  - [Setup](#setup)
+  - [Test](#test)
+- [👥 Authors](#authors)
+- [🤝 Contributing](#contributing)
+- [⭐️ Show your support](#support)
+- [🙏 Acknowledgements](#acknowledgements)
+- [📝 License](#license)
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 ES6 ToDo List <a name="about-project"></a>
+
+**ES6 ToDo List** is a modern and efficient task management application that leverages the power of ES6 and contemporary web technologies. With its intuitive interface, prioritization options, task categorization, and collaboration features, it offers a seamless experience for organizing and accomplishing daily tasks.
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+<details>
+  <summary>Client</summary>
+  <ul>
+    <li><a href="https://www.w3schools.com/html/">HTML</a></li>
+    <li><a href="https://www.w3schools.com/css/">CSS</a></li>
+    <li><a href="https://www.w3schools.com/js/">JavaScript ES6</a></li>
+    <li><a href="https://webpack.js.org/">Webpack</a></li>
+  </ul>
+</details>
+
+<!-- Features -->
+
+### Key Features <a name="key-features"></a>
+
+- Add new task with index, description and completed state.
+- Display a list of saved tasks.
+- Remove tasks from the list.
+- Edit tasks from the list.
+- Store book data using `localStorage`.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Future Features <a name="future-features"></a>
+
+- **Implement a CSS Preprocessor.**
+- **Implement a Database**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+To get a local copy up and running, follow these steps.
+
+### Setup
+
+To get a local copy up and running, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/RysthCraft/ES6-To-Do-List.git`
+2. Navigate to the project directory: `cd ES6-To-Do-List`
+3. Install the dependencies: `npm install`
+4. Build the project: `npm run build`
+5. Open `index.html` in your web browser.
+
+Then, you will change everything you want!
+
+### Test
+
+In order to follow the Linters you could check them using this:
+
+```
+HTML Checker:
+npx hint .
+
+CSS Checker:
+npx stylelint "**/*.{css,scss}"
+
+JavaScript Checker:
+npx eslint .
+```
+
+#### Note
+
+Use at the end '--fix' to help you out with any error.
+
+## 👥 Authors <a name="authors"></a>
+
+👤 **Rysth**
+
+- GitHub: [@rysth](https://github.com/RysthCraft)
+- Linkedin: [@johnpalacios](https://www.linkedin.com/in/john-palacios-rysth/)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## ⭐️ Show your support <a name="support"></a>
+
+If you like this project feel free to watch it and take some features that will enhance your project
+as well.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+I would like to thank the Microverse Team for reviewing my code and making me a better
+professional.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+## 📝 License <a name="license"></a>
+
+This project is [MIT](./LICENSE.md) licensed.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ src/index.js
@@ -0,0 +1,155 @@
+import { addTask, editTask, removeTask } from './modules/functionality.js';
+import {
+  markTaskComplete,
+  markTaskIncomplete,
+  removeAllCompleted,
+} from './modules/interactive.js';
+import './styles.css';
+
+let taskArray = JSON.parse(localStorage.getItem('taskArray')) || [];
+
+/* Add & Remove */
+const form = document.querySelector('#form');
+
+const updateLocalStorage = () => {
+  localStorage.setItem('taskArray', JSON.stringify(taskArray));
+};
+
+/* eslint-disable */
+
+const resetContent = () => {
+  updateLocalStorage();
+  traverseTasks();
+  form.reset();
+};
+
+/* eslint-enable */
+
+const addHoverEffect = () => {
+  const todoItems = document.querySelectorAll('.todo-item');
+
+  todoItems.forEach((item) => {
+    const input = item.querySelector('.todo-input');
+
+    if (item && input) {
+      input.addEventListener('mouseover', () => {
+        item.classList.add('bg-yellow');
+        input.classList.add('bg-yellow');
+      });
+
+      input.addEventListener('mouseout', () => {
+        item.classList.remove('bg-yellow');
+        input.classList.remove('bg-yellow');
+      });
+    }
+  });
+};
+
+const addRemoveFunction = () => {
+  const trashs = document.querySelectorAll('.btn-trash');
+
+  trashs.forEach((trash, index) => {
+    trash.addEventListener('click', () => {
+      removeTask(index, taskArray);
+      resetContent();
+    });
+  });
+};
+
+const addEditFunction = () => {
+  const todoInputs = document.querySelectorAll('.todo-input');
+
+  todoInputs.forEach((input, index) => {
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        editTask(index, input.value, taskArray);
+        resetContent();
+      }
+    });
+  });
+};
+
+const addMarkFunction = () => {
+  const todoChecks = document.querySelectorAll('.todo-check');
+
+  todoChecks.forEach((check, index) => {
+    check.addEventListener('change', (event) => {
+      if (event.target.checked) {
+        markTaskComplete(index, taskArray);
+      } else {
+        markTaskIncomplete(index, taskArray);
+      }
+      resetContent();
+    });
+  });
+};
+
+form.addEventListener('submit', (event) => {
+  const description = document.querySelector('#form-task').value;
+  if (description !== '') {
+    addTask(taskArray.length, description.trim(), taskArray);
+    resetContent();
+  }
+  event.preventDefault();
+});
+
+const traverseTasks = () => {
+  const todoList = document.querySelector('#todo-list');
+
+  if (taskArray) {
+    const contentHTML = taskArray
+      .map((task, index) => {
+        const checked = task.complete ? 'checked' : '';
+        const className = checked ? 'line-trough' : '';
+        const mustDelete = checked ? 'btn-trash' : '';
+        const icon = checked ? 'fa-trash' : 'fa-ellipsis-v';
+
+        return `
+        <li class="todo-item item-flex" id="todo-item-${index}">
+          <div>
+            <input class="form-check todo-check" type="checkbox" name="todo-input-${index}" id="todo-input-${index}" ${checked}/>
+            <input class="form-input todo-input ${className}" type="text" value="${task.description}" />
+          </div>
+           <button class="btn btn-icon ${mustDelete}" type="button" ><i class="fas ${icon}"></i></button>
+        </li>
+    `;
+      })
+      .join('');
+
+    const clearButton = `
+        <li class="todo-item bg-white-off" >
+           <button id="btn-clear" class="btn btn-clear color-gray w-100 text-center" type="button" >Clear all completed</button>
+        </li>
+    `;
+
+    todoList.innerHTML = contentHTML + clearButton;
+
+    const buttonClear = document.querySelector('#btn-clear');
+
+    buttonClear.addEventListener('click', () => {
+      removeAllCompleted(taskArray);
+      resetContent();
+    });
+
+    addHoverEffect();
+    addRemoveFunction();
+    addEditFunction();
+    addMarkFunction();
+  }
+};
+
+window.addEventListener('load', traverseTasks);
+
+/* Reset Animation */
+
+const resetButton = document.querySelector('#btn-reset');
+
+resetButton.addEventListener('click', () => {
+  resetButton.classList.add('btn-reset');
+  taskArray = [];
+  resetContent();
+
+  setTimeout(() => {
+    resetButton.classList.remove('btn-reset');
+  }, 500);
+});
